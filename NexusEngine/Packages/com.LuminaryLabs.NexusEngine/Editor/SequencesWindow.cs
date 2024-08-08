036bcdbf939a23d17779ece1869cf20b71212575
Crimson Wheeler <crimson.wheeler@gmail.com>
2024-08-07 23:32:50 -0400

    Fixed null super sequence, core sequence and sequence viewer

--- NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Editor/SequencesWindow.cs
+++ NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Editor/SequencesWindow.cs
@@ -2,35 +2,195 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using LuminaryLabs.NexusEngine;
+using System;
 
 public class SequencesWindow : EditorWindow
 {
-    // private List<SequenceState> loadedStates = new List<SequenceState>();
     private Vector2 scrollPos;
+    private Dictionary<Guid, bool> foldoutStates = new Dictionary<Guid, bool>();
+    List<SerializableSequence> rootSequences = new List<SerializableSequence>();
 
-    public List<ISequence> sequences => Sequence.GetAll();
+    // Retrieve the list of all running sequences
+    public List<ISequence> sequences = new List<ISequence>();
 
-    [MenuItem("Tools/Sequences")]
+    [MenuItem("LuminaryLabs/Sequence Viewer")]
     public static void ShowWindow()
     {
         GetWindow<SequencesWindow>("Sequences");
     }
 
+    private void OnEnable()
+    {
+        // Get all sequences in the scene
+
+        Debug.Log("SequencesWindow enabled");
+        // Get all sequences in the scene
+        sequences = Sequence.GetAll();
+        rootSequences = BuildSequenceHierarchy(sequences);
+    }
+
+
+
     private void OnGUI()
     {
 
+        if (Application.isPlaying == false)
+        {
+            GUILayout.Label("Please enter play mode to view running sequences.", EditorStyles.label);
+
+            if (GUILayout.Button("Enter Player Mode"))
+            {
+                EditorApplication.isPlaying = true;
+            }
+            return;
+        }
+
+        if (GUILayout.Button("Refresh Sequences", GUILayout.Width(200)))
+        {
+            // Get all sequences in the scene
+            sequences = Sequence.GetAll();
+            rootSequences = BuildSequenceHierarchy(sequences);
+        }
+
+        if (sequences == null || sequences.Count == 0)
+        {
+            GUILayout.Label("No sequences are currently running.", EditorStyles.label);
+            return;
+        }
+
+        GUILayout.Label("Running Sequences", EditorStyles.boldLabel);
+
+        // Start a scroll view
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, true);
+
+        // Build and display the sequence hierarchy
+
+        foreach (var rootSequence in rootSequences)
+        {
+            rootSequence.Draw();
+        }
+
+        EditorGUILayout.EndScrollView();
     }
 
-    private void SaveSequenceStates()
+    private List<SerializableSequence> BuildSequenceHierarchy(List<ISequence> sequences)
     {
-        // Sequence.Save();
-        Debug.Log("Sequence states saved.");
+        var sequenceDict = new Dictionary<ISequence, SerializableSequence>();
+        var rootSequences = new List<SerializableSequence>();
+
+        // Create SerializableSequence instances and store them in a dictionary
+        foreach (var sequence in sequences)
+        {
+            var serializableSequence = new SerializableSequence(sequence);
+            sequenceDict[sequence] = serializableSequence;
+        }
+
+        // Build the hierarchy by assigning children to their respective parents
+        foreach (var sequence in sequences)
+        {
+            var serializableSequence = sequenceDict[sequence];
+            bool isRootSequence = sequence.superSequence == null;
+            bool isChildSequence = !isRootSequence && sequenceDict.ContainsKey(sequence.superSequence);
+
+            if (isRootSequence)
+            {
+                rootSequences.Add(serializableSequence);
+            }
+            else if (isChildSequence)
+            {
+                var parentSequence = sequenceDict[sequence.superSequence];
+                parentSequence.children.Add(serializableSequence);
+            }
+        }
+
+        Debug.Log($"Root sequences count: {rootSequences.Count}");
+        return rootSequences;
     }
 
-    private void LoadSequenceStates()
+    class SerializableSequence
     {
-        // loadedStates = SequenceStateSaver.LoadSequenceStates();
-        Debug.Log("Sequence states loaded.");
-    }
+        public ISequence sequence;
+        public List<SerializableSequence> children;
+        private Dictionary<Guid, bool> foldoutStates = new Dictionary<Guid, bool>();
 
+        public SerializableSequence(ISequence sequence)
+        {
+            this.sequence = sequence;
+            children = new List<SerializableSequence>();
+        }
+
+        public void Draw()
+        {
+            if (sequence == null)
+            {
+                return;
+            }
+
+            // Handle the foldout for the sequence
+            Guid key = sequence.guid;
+            bool isExpanded = GetFoldoutState(key);
+
+            string label = GetSequenceLabel();
+            isExpanded = EditorGUILayout.Foldout(isExpanded, label, true);
+
+            SetFoldoutState(key, isExpanded);
+
+            if (isExpanded)
+            {
+                EditorGUI.indentLevel++;
+
+                // Show the information of the current sequence
+                DrawSequenceInfo();
+
+                GUILayout.Space(10);
+
+                // Draw children
+                foreach (var child in children)
+                {
+                    child.Draw();
+                }
+
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        private string GetSequenceLabel()
+        {
+            if (sequence is MonoBehaviour monoBehaviour)
+            {
+                return $"Sequence: {monoBehaviour.name}";
+            }
+            else
+            {
+                return $"Sequence: {sequence.GetType().Name}";
+            }
+        }
+
+        private bool GetFoldoutState(Guid superSequenceKey)
+        {
+            if (!foldoutStates.ContainsKey(superSequenceKey))
+                foldoutStates[superSequenceKey] = true;
+            return foldoutStates[superSequenceKey];
+        }
+
+        private void SetFoldoutState(Guid superSequenceKey, bool state)
+        {
+            foldoutStates[superSequenceKey] = state;
+        }
+
+        private void DrawSequenceInfo()
+        {
+            EditorGUI.indentLevel++;
+
+            GUILayout.BeginVertical(EditorStyles.helpBox);
+
+            GUILayout.Label($"GUID: {sequence.guid}", EditorStyles.boldLabel);
+            GUILayout.Label($"Current Data: {sequence.currentData ?? "No Data"}", EditorStyles.label);
+            GUILayout.Label($"Super Sequence: {(sequence.superSequence != null ? sequence.superSequence.guid.ToString() : "None")}", EditorStyles.label);
+
+            GUILayout.EndVertical();
+
+            EditorGUI.indentLevel--;
+        }
+    }
 }
