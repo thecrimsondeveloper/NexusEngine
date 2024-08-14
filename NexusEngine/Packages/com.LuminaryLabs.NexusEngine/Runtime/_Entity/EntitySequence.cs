dd7a9f4e95e15e7630d2439ee1d7f5d9a49a3660
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2024-08-14 04:10:38 -0400

    Entity (#12)

--- NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Runtime/_Entity/EntitySequence.cs
+++ NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Runtime/_Entity/EntitySequence.cs
@@ -3,38 +3,57 @@ using Cysharp.Threading.Tasks;
 using LuminaryLabs.Samples;
 using UnityEngine;
 
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
 namespace LuminaryLabs.NexusEngine
 {
-    public class EntitySequence : MonoSequence
+    public abstract class EntitySequence<T> : MonoSequence where T : EntitySequenceData
     {
-        [SerializeField] private MoveHandler movementHandler;
-        [SerializeField] private VisualsHandler visualsHandler;
 
+
+#if ODIN_INSPECTOR 
+        [SerializeField, HideLabel, BoxGroup("CORE DATA")]
+#endif
+
+
+        private T _currentData;
+
+        public new T currentData
+        {
+            get => base.currentData != null ? (T)base.currentData : _currentData;
+            set => _currentData = value;
+        }
+
+        protected virtual void Start()
+        {
+            Sequence.Run(this, new SequenceRunData
+            {
+                sequenceData = currentData,
+                superSequence = null,
+            });
+        }
 
         protected override UniTask Initialize(object currentData = null)
         {
+            if (currentData is T data)
+            {
+
+
+
+                return Initialize(data);
+            }
             return UniTask.CompletedTask;
         }
 
-        protected override void OnBegin()
-        {
-
-        }
-
-        protected override UniTask Unload()
-        {
-
-
-            return UniTask.CompletedTask;
-        }
-
-
+        protected abstract UniTask Initialize(T currentData);
     }
 
+    [System.Serializable]
     public class EntitySequenceData
     {
-        public MoveHandler movementHandlerPrefab;
-        public MoveHandlerData movementHandlerData;
-        public VisualsHandler visualsHandlerPrefab;
+
     }
+
 }
