718e71e716c590dca61ded96f438eb4b732cb6a2
Crimson Wheeler <crimson.wheeler@gmail.com>
2024-08-26 11:39:16 -0400

    Sequence Generator

--- NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Editor/SequenceTemplateScript/TemplateSequence_WithType.cs
+++ NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Editor/SequenceTemplateScript/TemplateSequence_WithType.cs
@@ -8,14 +8,36 @@ namespace YourNamespace
 {
     public class TemplateClass : TemplateBaseClass<TemplateClassData>
     {
+
+        #region SubSequences
+        #endregion
+
+
+        #region SubSequenceData
+        #endregion
+
+
+        #region Sequence Logic
         protected override UniTask Initialize(TemplateClassData currentData)
         {
+
             // Initialization logic here
             return UniTask.CompletedTask;
         }
 
         protected override void OnBegin()
         {
+            #region Template Sequence Run
+            // Run a sequence
+            ISequence sequence = null;
+            object data = null;
+            Sequence.Run(sequence, new SequenceRunData
+            {
+                superSequence = this,
+                sequenceData = data,
+                onFinished = TemplateFinished
+            });
+            #endregion
             // Logic to execute when the sequence begins
         }
 
@@ -24,11 +46,39 @@ namespace YourNamespace
             // Cleanup logic here
             return UniTask.CompletedTask;
         }
+
+        #endregion
+
+
+
+        #region SequenceFinishFunctions 
+        void TemplateFinished(ISequence sequence)
+        {
+
+        }
+        #endregion
     }
+
+    #region Data Class
 
     [System.Serializable]
     public class TemplateClassData
     {
+
+        #region MonoSequnces
+
+        #endregion
+
+        #region ScriptableSequences
+
+        #endregion
+
+        #region Data Fields
+
+        #endregion
+
         // Define data fields here
     }
+
+    #endregion
 }
