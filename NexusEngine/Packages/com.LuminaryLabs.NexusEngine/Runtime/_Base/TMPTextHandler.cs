7f51bb1af2bde478e13fb8c9da936b03f0dc1b81
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2024-11-07 19:07:13 -0500

    Create Text Handler

--- NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Runtime/_Base/TMPTextHandler.cs
+++ NexusEngine/Packages/com.LuminaryLabs.NexusEngine/Runtime/_Base/TMPTextHandler.cs
@@ -2,28 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using LuminaryLabs.NexusEngine;
+using TMPro;
 using UnityEngine;
 
 public class TMPTextHandler : BaseSequence<TMPTextHandlerData>
 {
     // TextMeshProUGUI textMeshPro;
+    private TMP_Text textToChange;
     private string text;
     private float timePerCharacter;
     protected override UniTask Initialize(TMPTextHandlerData currentData)
     {
+        textToChange = currentData.textToChange;
         text = currentData.text;
         timePerCharacter = currentData.timePerCharacter;
         return UniTask.CompletedTask;
     }
-    protected override void OnBegin()
+    protected override async void OnBegin()
     {
-        throw new System.NotImplementedException();
+        textToChange.text = ""; 
+        
+
+        //use a for loop to add each character to the text
+        for (int i = 0; i < text.Length; i++)
+        {
+            if(Application.isPlaying == false)
+            {
+                return;
+            }
+            textToChange.text += text[i];
+            await UniTask.Delay((int)(timePerCharacter * 1000));
+        }
+
+        this.Complete();
     }
 }
 
 public class TMPTextHandlerData : BaseSequenceData
 {
-    // public TextMeshProUGUI textMeshPro;
+    public TMP_Text textToChange;
     public string text;
     public float timePerCharacter;
 }
